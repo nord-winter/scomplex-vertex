a6ab9ef85d17e319a32fc8bbea23c06360d49c21
Vladislav Simutin <vlad.sn.s0n@gmail.com>
2024-09-04 14:29:49 +0700

    superform adapter

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	build: {
+    	rollupOptions: {
+      		external: [
+        		'sveltekit-superforms/adapters' // Указываем внешний модуль
+      		]
+    	}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
