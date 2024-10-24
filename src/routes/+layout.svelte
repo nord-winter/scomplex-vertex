6339986678d63fcb3e1f41c9bbdf762bf9c8a828
Vladislav Simutin <vlad.sn.s0n@gmail.com>
2024-10-24 13:42:19 +0700

    Create ref link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,7 @@
 	import '$lib/vendor/custom/style.css';
 	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
+	import { refStore } from '../stores';
 	import scriptSrc from 'bootstrap/dist/js/bootstrap.bundle.js?url';
 	import Header from '$lib/components/Header.svelte';
 	import ToggleScroll from '$lib/components/ToggleScroll.svelte';
@@ -29,6 +30,18 @@
 			duration: 1000,
 			once: true
 		});
+		const urlParams = new URLSearchParams(window.location.search);
+		const ref = urlParams.get('ref'); // Получаем параметр ref из URL
+		if (ref) {
+			refStore.set(ref); // Сохраняем ref в store
+			localStorage.setItem('ref', ref); // Сохраняем ref в localStorage
+		} else {
+			// Если ref отсутствует в URL, пытаемся восстановить его из localStorage
+			const storedRef = localStorage.getItem('ref');
+			if (storedRef) {
+				refStore.set(storedRef);
+			}
+		}
 	});
 </script>
 
@@ -42,21 +55,42 @@
 	<slot />
 	<ToggleScroll />
 	<Footer />
-<!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();
-   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript">
+		(function (m, e, t, r, i, k, a) {
+			m[i] =
+				m[i] ||
+				function () {
+					(m[i].a = m[i].a || []).push(arguments);
+				};
+			m[i].l = 1 * new Date();
+			for (var j = 0; j < document.scripts.length; j++) {
+				if (document.scripts[j].src === r) {
+					return;
+				}
+			}
+			(k = e.createElement(t)),
+				(a = e.getElementsByTagName(t)[0]),
+				(k.async = 1),
+				(k.src = r),
+				a.parentNode.insertBefore(k, a);
+		})(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
 
-   ym(98461054, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-   });
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/98461054" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
+		ym(98461054, 'init', {
+			clickmap: true,
+			trackLinks: true,
+			accurateTrackBounce: true,
+			webvisor: true
+		});
+	</script>
+	<noscript
+		><div>
+			<img
+				src="https://mc.yandex.ru/watch/98461054"
+				style="position:absolute; left:-9999px;"
+				alt=""
+			/>
+		</div></noscript
+	>
+	<!-- /Yandex.Metrika counter -->
 </main>
--- src/stores.js
+++ src/stores.js
@@ -2,3 +2,5 @@
 import { writable } from 'svelte/store';
 
 export const selectedProduct = writable(null);
+
+export const refStore = writable('');
